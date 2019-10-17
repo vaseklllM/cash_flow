553b9bbb363740bb992b137c62862e4ce5fe5a5e
vaseklllm <vasia.motnin@gmail.com>
2019-10-17 22:12:59 +0300

    debug

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,35 @@
 import React from "react"
 import { ActiveTable, PasiveTable, CostsTable, IncomeTable } from "./Containers"
 import { NavBar } from "./Containers"
-import { Box, Grid, Container } from "@material-ui/core"
+import { Box, Grid, Container, Hidden } from "@material-ui/core"
 import "./App.scss"
 
 const App = () => {
-    console.log(window);
+    console.log(window)
     return (
         <>
             <NavBar />
             <Container maxWidth='xl'>
                 <Box mt={2}>
                     <Grid container spacing={3}>
-                        <Grid item xs={12} md={6} xl={4}>
-                            <IncomeTable />
+                        <Grid item container spacing={3} xl={8}>
+                            <Grid item xs={12} md={6}>
+                                <IncomeTable />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <CostsTable />
+                            </Grid>
+                            <Grid item xs={12} md={12} lg={6}>
+                                <ActiveTable />
+                            </Grid>
+                            <Grid item xs={12} md={12} lg={6}>
+                                <PasiveTable />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} md={6} xl={4}>
-                            <CostsTable />
-                        </Grid>
-                        <Grid item xs={12} md={12} lg={6} xl={4}>
-                            <ActiveTable />
-                        </Grid>
-                        <Grid item xs={12} md={12} lg={6} xl={4}>
-                            <PasiveTable />
+                        <Grid item xs={12} xl={4}>
+                            <Grid>
+                                <ActiveTable /> {/* вся таблиця */}
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Box>
