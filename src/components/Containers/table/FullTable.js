2085fcd2827f056c4a29fc93935d7fd64f1aab24
vaseklllm <vasia.motnin@gmail.com>
2019-10-27 22:22:14 +0200

    convert full table from func to class

--- src/components/Containers/table/FullTable.js
+++ src/components/Containers/table/FullTable.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import {
     Box,
@@ -8,23 +8,12 @@ import {
     TableHead,
     TableBody,
     Table,
-    makeStyles,
     Checkbox
 } from "@material-ui/core"
 import { StyledTableCell, StyledTableRow } from "../../Creators/Table/utils"
 import { Loader } from "../../pages"
 import { maths, showDate, retentionTime } from "../../utils"
 import { setCheckBox } from "../../../store/serverMoney/action"
-
-const useStyles = makeStyles(() => ({
-    root: {
-        width: "100%",
-        overflowX: "auto"
-    },
-    table: {
-        minWidth: 700
-    }
-}))
 
 const bodyText = {
     title: "Вся таблиця",
@@ -42,106 +31,127 @@ const bodyText = {
     ]
 }
 
-function FullTable({ cashFlow, setCheckBox }) {
-    const classes = useStyles()
-    const row = bodyText.collumn.map((item, index) => {
-        if (!index || index === 1) {
-            return <StyledTableCell key={index}>{item}</StyledTableCell>
-        }
-        return (
-            <StyledTableCell key={index} align='right'>
-                {item}
-            </StyledTableCell>
-        )
-    })
-    let bodyTable
-    if (cashFlow) {
-        bodyTable = cashFlow.map((item, index) => {
-            const {
-                name,
-                price,
-                currency,
-                income,
-                pcs,
-                dateBuy,
-                checked
-            } = item
+class FullTable extends Component {
+    state = {
+        onCheck: [1]
+    }
+    render() {
+        console.log(this.state.onCheck)
+        const { cashFlow, setCheckBox } = this.props
+        const row = bodyText.collumn.map((item, index) => {
+            if (!index || index === 1) {
+                return <StyledTableCell key={index}>{item}</StyledTableCell>
+            }
             return (
-                <StyledTableRow
-                    hover
-                    key={item.id}
-                    onClick={() => {
-                        setCheckBox(item.id)
-                    }}
-                    style={
-                        checked
-                            ? { backgroundColor: "rgba(0, 0, 0, 0.15)" }
-                            : null
-                    }
-                >
-                    <StyledTableCell padding='checkbox'>
-                        <Checkbox checked={false} />
-                    </StyledTableCell>
-                    <StyledTableCell component='th' scope='row'>
-                        {name}
-                    </StyledTableCell>
-                    <StyledTableCell align='right'>
-                        {showDate(dateBuy)}
-                    </StyledTableCell>
-                    <StyledTableCell align='right'>
-                        {retentionTime(dateBuy)}
-                    </StyledTableCell>
-                    <StyledTableCell align='right'>
-                        {price ? `${price} ${currency}` : "-"}
-                    </StyledTableCell>
-                    <StyledTableCell align='right'>{`${pcs} шт.`}</StyledTableCell>
-                    <StyledTableCell align='right'>
-                        {income ? `${income} ${currency}` : "-"}
-                    </StyledTableCell>
-                    <StyledTableCell align='right'>
-                        {price * pcs ? `${price * pcs} ${currency}` : "-"}
-                    </StyledTableCell>
-                    <StyledTableCell align='right'>
-                        {maths.roi(income, price * pcs) !== 0 &&
-                        maths.roi(income, price * pcs) !== -Infinity
-                            ? `${maths.roi(income, price * pcs)} %`
-                            : "-"}
+                <StyledTableCell key={index} align='right'>
+                    {item}
+                </StyledTableCell>
+            )
+        })
+        let bodyTable
+        if (cashFlow) {
+            bodyTable = cashFlow.map(item => {
+                const {
+                    name,
+                    price,
+                    currency,
+                    income,
+                    pcs,
+                    dateBuy,
+                    checked
+                } = item
+                return (
+                    <StyledTableRow
+                        hover
+                        key={item.id}
+                        onClick={() => {
+                            setCheckBox(item.id)
+                        }}
+                        style={
+                            checked
+                                ? { backgroundColor: "rgba(0, 0, 0, 0.15)" }
+                                : null
+                        }
+                    >
+                        <StyledTableCell padding='checkbox'>
+                            <Checkbox
+                                checked={false}
+                                onClick={e => {
+                                    e.stopPropagation()
+                                    this.setState({
+                                        onCheck: [
+                                            ...this.state.onCheck,
+                                            item.id
+                                        ]
+                                    })
+                                }}
+                            />
+                        </StyledTableCell>
+                        <StyledTableCell component='th' scope='row'>
+                            {name}
+                        </StyledTableCell>
+                        <StyledTableCell align='right'>
+                            {showDate(dateBuy)}
+                        </StyledTableCell>
+                        <StyledTableCell align='right'>
+                            {retentionTime(dateBuy)}
+                        </StyledTableCell>
+                        <StyledTableCell align='right'>
+                            {price ? `${price} ${currency}` : "-"}
+                        </StyledTableCell>
+                        <StyledTableCell align='right'>{`${pcs} шт.`}</StyledTableCell>
+                        <StyledTableCell align='right'>
+                            {income ? `${income} ${currency}` : "-"}
+                        </StyledTableCell>
+                        <StyledTableCell align='right'>
+                            {price * pcs ? `${price * pcs} ${currency}` : "-"}
+                        </StyledTableCell>
+                        <StyledTableCell align='right'>
+                            {maths.roi(income, price * pcs) !== 0 &&
+                            maths.roi(income, price * pcs) !== -Infinity
+                                ? `${maths.roi(income, price * pcs)} %`
+                                : "-"}
+                        </StyledTableCell>
+                    </StyledTableRow>
+                )
+            })
+        } else {
+            bodyTable = (
+                <StyledTableRow>
+                    <StyledTableCell colSpan={bodyText.collumn.length}>
+                        <Loader />
                     </StyledTableCell>
                 </StyledTableRow>
             )
-        })
-    } else {
-        bodyTable = (
-            <StyledTableRow>
-                <StyledTableCell colSpan={bodyText.collumn.length}>
-                    <Loader />
-                </StyledTableCell>
-            </StyledTableRow>
+        }
+
+        return (
+            <>
+                <Box p={0}>
+                    <Typography variant='h5' gutterBottom>
+                        {bodyText.title}
+                    </Typography>
+                </Box>
+                <Paper
+                    style={{
+                        width: "100%",
+                        overflowX: "auto"
+                    }}
+                >
+                    <Table
+                        size='small'
+                        aria-label='a dense table'
+                        style={{ minWidth: "1250px" }}
+                    >
+                        <TableHead>
+                            <TableRow>{row}</TableRow>
+                        </TableHead>
+                        <TableBody>{bodyTable}</TableBody>
+                    </Table>
+                </Paper>
+            </>
         )
     }
-
-    return (
-        <>
-            <Box p={0}>
-                <Typography variant='h5' gutterBottom>
-                    {bodyText.title}
-                </Typography>
-            </Box>
-            <Paper className={classes.root}>
-                <Table
-                    className={classes.table}
-                    size='small'
-                    aria-label='a dense table'
-                    style={{ minWidth: "1250px" }}
-                >
-                    <TableHead>
-                        <TableRow>{row}</TableRow>
-                    </TableHead>
-                    <TableBody>{bodyTable}</TableBody>
-                </Table>
-            </Paper>
-        </>
-    )
 }
 
 const mapStateToProps = ({ serverMoney }) => ({
