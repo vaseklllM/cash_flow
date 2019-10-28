480f1740cfd92210188407d19e169b98ad1dd586
vaseklllm <vasia.motnin@gmail.com>
2019-10-28 22:37:09 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import {
     ProgressBarCapital,
     IncomeToCosts
 } from "./Containers"
-import { Grid, Container, Box } from "@material-ui/core"
+import { Grid, Container, Box, Typography } from "@material-ui/core"
 import "./App.scss"
 
 const App = () => {
@@ -30,11 +30,47 @@ const App = () => {
                 <div className='separator-horizontal' />
                 <Grid container justify='space-between'>
                     <Grid item container spacing={3}>
-                        <Grid item xs={12} md={6}>
+                        <Grid item xs={12} lg={6} xl={3}>
                             <IncomeTable />
                         </Grid>
-                        <Grid item xs={12} md={6}>
+                        <Grid item xs={12} lg={6} xl={3}>
                             <CostsTable />
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            lg={6}
+                            xl={3}
+                            direction='row'
+                            justify='center'
+                            alignItems='center'
+                            container
+                        >
+                            <Typography
+                                variant='h4'
+                                style={{ margin: 0 }}
+                                gutterBottom
+                            >
+                                графік капиталу
+                            </Typography>
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            lg={6}
+                            xl={3}
+                            direction='row'
+                            justify='center'
+                            alignItems='center'
+                            container
+                        >
+                            <Typography
+                                variant='h4'
+                                style={{ margin: 0 }}
+                                gutterBottom
+                            >
+                                графік доходу
+                            </Typography>
                         </Grid>
                         <Grid item xs={12} lg={12} xl={6}>
                             <ActiveTable />
