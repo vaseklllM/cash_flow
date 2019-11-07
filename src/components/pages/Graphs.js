a71fe9933d08818f61e01af3f87aeaab9054a8a5
vaseklllm <vasia.motnin@gmail.com>
2019-11-07 19:20:38 +0200

    йцукен

--- src/components/pages/Graphs.js
+++ src/components/pages/Graphs.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import graph from "../Creators/graph"
-// import { Grid, Box, Container } from "@material-ui/core"
 import "./Graphs.scss"
 
 class Graphs extends Component {
@@ -15,42 +14,24 @@ class Graphs extends Component {
         return (
             <div className='GraphsContainer'>
                 <div className='GraphsContainer-graphs'>
-                    <div className='GraphsContainer-graphs-graph' id='graph1'></div>
-                    <div className='GraphsContainer-graphs-graph' id='graph2'></div>
-                    <div className='GraphsContainer-graphs-graph' id='graph3'></div>
-                    <div className='GraphsContainer-graphs-graph' id='graph4'></div>
+                    <div
+                        className='GraphsContainer-graphs-graph'
+                        id='graph1'
+                    ></div>
+                    <div
+                        className='GraphsContainer-graphs-graph'
+                        id='graph2'
+                    ></div>
+                    <div
+                        className='GraphsContainer-graphs-graph'
+                        id='graph3'
+                    ></div>
+                    <div
+                        className='GraphsContainer-graphs-graph'
+                        id='graph4'
+                    ></div>
                 </div>
             </div>
-            // <Container maxWidth='xl'>
-            //     <Box mt={10}>
-            //         <Grid container spacing={3}>
-            //             <Grid item md={6} lg={6} xl={4}>
-            //                 <div
-            //                     style={{ width: "max-content" }}
-            //                     id='graph'
-            //                 ></Container>
-            //             </Grid>
-            //             <Grid item md={6} lg={6} xl={4}>
-            //                 <div
-            //                     style={{ width: "max-content" }}
-            //                     id='graph1'
-            //                 ></div>
-            //             </Grid>
-            //             <Grid item md={6} lg={6} xl={4}>
-            //                 <div
-            //                     style={{ width: "max-content" }}
-            //                     id='graph2'
-            //                 ></div>
-            //             </Grid>
-            //             <Grid item md={6} lg={6} xl={4}>
-            //                 <div
-            //                     style={{ width: "max-content" }}
-            //                     id='graph3'
-            //                 ></div>
-            //             </Grid>
-            //         </Grid>
-            //     </Box>
-            // </Container>
         )
     }
 }
